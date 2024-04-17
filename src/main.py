8aea27623d40db848ce939786d8b1140ecf2a86b
rfreemanjr <rdfreejr@gmail.com>
2024-04-17 00:43:57 -0400

    next steps added to project

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+
+def main():
+    t_node = TextNode("Potatoe text", "bold", "www.google.com")
+    print(t_node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,12 @@
+class TextNode():
+    
+    def __init__(self, text, type, url):
+        self.text = text
+        self.type = type
+        self.url = url
+
+def __eq__():
+    return (self.text == self.type and self.text == self.url)
+
+def __repr__():
+    return f"TextNode({self.text}, {self.type}, {self.url})"
